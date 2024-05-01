5adcc4d9389461a16214ca46871109ba9e4f0bb5
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-05-01 08:24:27 -0700

    remove log

--- demo/with-next/app/api/prepare-payment/route.ts
+++ demo/with-next/app/api/prepare-payment/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const GET = (req: NextRequest) => {
   const reference = generateReferenceId();
-  console.log(reference);
   // Save this reference ID in the DB to compare the return result to
   // ...
   return NextResponse.json({
