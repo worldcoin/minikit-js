802b02980bc52808fe5705fbdba0805d047da9f0
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-07-04 15:35:55 -0700

    fixes

--- src/helpers/payment/client.ts
+++ src/helpers/payment/client.ts
@@ -1,4 +1,4 @@
-import { Tokens } from "types";
+import { PayCommandInput, Tokens } from "types";
 import { PaymentErrorMessage } from "types/errors";
 import { TokenDecimals } from "types/payment";
 import { MiniAppPaymentErrorPayload } from "types/responses";
@@ -22,4 +22,22 @@ export const tokenToDecimals = (amount: number, token: Tokens): number => {
     throw new Error(`The resulting amount is not a whole number: ${result}`);
   }
   return result;
+};
+
+export const validatePaymentPayload = (payload: PayCommandInput): boolean => {
+  if (
+    payload.tokens.some(
+      (token) => token.symbol == "USDCE" && parseFloat(token.token_amount) < 0.1
+    )
+  ) {
+    console.error("USDCE amount should be greater than $0.1");
+    return false; // reject
+  }
+
+  if (payload.reference.length > 36) {
+    console.error("Reference must not exceed 36 characters");
+    return false;
+  }
+
+  return true; // accept
 };
