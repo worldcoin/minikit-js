687487b6b3c7ee3899ec4f08912f775f5d1810d8
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-09-25 13:41:04 +0200

    update links

--- demo/with-next/components/ClientContent/ExternalLinks.tsx
+++ demo/with-next/components/ClientContent/ExternalLinks.tsx
@@ -6,29 +6,37 @@ export const ExternalLinks = () => {
       <p className="text-xl font-bold">Test External Links</p>
       <div className="grid md:grid-cols-2 gap-4">
         <Link
-          href="https://worldcoin.org/apps"
+          href="https://worldcoin.org/world-chain"
           className="bg-green-500 text-white text-center rounded-lg p-3"
         >
           Valid Associated Domain (Link)
         </Link>
         <button
-          onClick={() => window.open("https://worldcoin.org/apps")}
+          onClick={() => window.open("https://worldcoin.org/world-chain")}
           className="text-white bg-green-500 hover:bg-blue-300 transition p-4 leading-[1] rounded-lg"
         >
           Valid Associated Domain (Button)
         </button>
         <Link
-          href="https://whitepaper.worldcoin.org"
+          href="https://docs.worldcoin.org"
           className="bg-green-500 text-white text-center rounded-lg p-3"
         >
           Valid Subdomain (Link)
         </Link>
         <button
-          onClick={() => window.open("https://whitepaper.worldcoin.org")}
+          onClick={() => window.open("https://docs.worldcoin.org")}
           className="text-white bg-green-500 transition p-4 leading-[1] rounded-lg"
         >
           Valid Subdomain (Button)
         </button>
+        <iframe
+          src="https://docs.worldcoin.org/reference/api"
+          className="border-0 w-full h-72"
+        />
+        <iframe
+          src="https://developer.worldcoin.org/"
+          className="border-0 w-full h-72"
+        />
 
         <button
           onClick={() => window.open("https://google.com", "_blank")}
