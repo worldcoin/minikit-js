8a9e982bc13f32fb3dca750cc0d2f20bd6b83f27
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-09-25 16:18:59 +0200

    0.31 Minikit: Improve init logic (#70)
    * update links
    
    * double sub domain
    
    * re organise install

--- demo/with-next/components/ClientContent/ExternalLinks.tsx
+++ demo/with-next/components/ClientContent/ExternalLinks.tsx
@@ -6,28 +6,44 @@ export const ExternalLinks = () => {
       <p className="text-xl font-bold">Test External Links</p>
       <div className="grid md:grid-cols-2 gap-4">
         <Link
-          href="https://worldcoin.org/apps"
+          href="https://worldcoin.org/world-chain"
           className="bg-green-500 text-white text-center rounded-lg p-3"
         >
           Valid Associated Domain (Link)
         </Link>
         <button
-          onClick={() => window.open("https://worldcoin.org/apps")}
+          onClick={() => window.open("https://worldcoin.org/world-chain")}
           className="text-white bg-green-500 hover:bg-blue-300 transition p-4 leading-[1] rounded-lg"
         >
           Valid Associated Domain (Button)
         </button>
         <Link
-          href="https://whitepaper.worldcoin.org"
+          href="https://docs.worldcoin.org"
           className="bg-green-500 text-white text-center rounded-lg p-3"
         >
           Valid Subdomain (Link)
         </Link>
         <button
-          onClick={() => window.open("https://whitepaper.worldcoin.org")}
+          onClick={() => window.open("https://docs.worldcoin.org")}
           className="text-white bg-green-500 transition p-4 leading-[1] rounded-lg"
         >
           Valid Subdomain (Button)
+        </button>
+        <Link
+          href="https://worldcoin-developer-portal.eu.auth0.com/u/login/identifier?state=hKFo2SBHakd3VGFGMllRb0E4QVJMY3pDeS04VG5lWGNwTFVkdKFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIEpFNTd0NjdIOTZLVzdVMG5FVXdrQ0dHUV92dHVwaE1ho2NpZNkgbU1vVGJqbFI4RmZYVEJrU05nVzk3d1BxR2tmUmNWUk8"
+          className="bg-green-500 text-white text-center rounded-lg p-3"
+        >
+          Valid double Subdomain (Link)
+        </Link>
+        <button
+          onClick={() =>
+            window.open(
+              "https://worldcoin-developer-portal.eu.auth0.com/u/login/identifier?state=hKFo2SBHakd3VGFGMllRb0E4QVJMY3pDeS04VG5lWGNwTFVkdKFur3VuaXZlcnNhbC1sb2dpbqN0aWTZIEpFNTd0NjdIOTZLVzdVMG5FVXdrQ0dHUV92dHVwaE1ho2NpZNkgbU1vVGJqbFI4RmZYVEJrU05nVzk3d1BxR2tmUmNWUk8"
+            )
+          }
+          className="text-white bg-green-500 transition p-4 leading-[1] rounded-lg"
+        >
+          Valid double subdomain (Button)
         </button>
 
         <button
