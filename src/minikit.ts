894264e24ad6ad36658630ab61a437e3e6d9567a
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-01 10:09:56 -0700

    change to worldchain

--- src/minikit.ts
+++ src/minikit.ts
@@ -244,7 +244,7 @@ export class MiniKit {
         return null;
       }
 
-      const network = Network.Optimism; // MiniKit only supports Optimism for now
+      const network = Network.WorldChain; // MiniKit only supports Optimism for now
 
       const eventPayload: PayCommandPayload = {
         ...payload,
