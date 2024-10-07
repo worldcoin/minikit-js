6bf72029729e37c5f6ec63d4f40c6ab109e52cfd
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-06 23:11:32 -0700

    improve error

--- packages/react/src/transaction/index.ts
+++ packages/react/src/transaction/index.ts
@@ -9,20 +9,25 @@ export async function fetchTransactionHash(
   appConfig: AppConfig,
   transactionId: string
 ): Promise<TransactionStatus> {
-  const response = await fetch(
-    `https://staging-developer.worldcoin.org/api/v2/minikit/transaction/${transactionId}?app_id=${appConfig.app_id}&type=transaction`,
-    {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${appConfig.api_key}`,
-      },
-    }
-  );
+  try {
+    const response = await fetch(
+      `https://staging-developer.worldcoin.org/api/v2/minikit/transaction/${transactionId}?app_id=${appConfig.app_id}&type=transaction`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${appConfig.api_key}`,
+        },
+      }
+    );
 
-  if (!response.ok) {
+    if (!response.ok) {
+      throw new Error("Failed to fetch transaction status");
+    }
+
+    const data: TransactionStatus = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Error fetching transaction status", error);
     throw new Error("Failed to fetch transaction status");
   }
-
-  const data: TransactionStatus = await response.json();
-  return data;
 }
