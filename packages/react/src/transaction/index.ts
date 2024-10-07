39d16bf6fc207ec52a94584dfa42c7f289e25418
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-06 23:18:35 -0700

    check logs

--- packages/react/src/transaction/index.ts
+++ packages/react/src/transaction/index.ts
@@ -9,6 +9,7 @@ export async function fetchTransactionHash(
   appConfig: AppConfig,
   transactionId: string
 ): Promise<TransactionStatus> {
+  console.log("Fetching transaction status for:", transactionId);
   try {
     const response = await fetch(
       `https://staging-developer.worldcoin.org/api/v2/minikit/transaction/${transactionId}?app_id=${appConfig.app_id}&type=transaction`,
