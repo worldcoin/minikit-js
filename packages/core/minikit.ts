9d58e63b6f8e734c91f2cc7bf0934f0c6d1c054f
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2024-10-08 12:51:43 -0700

    test

--- packages/core/minikit.ts
+++ packages/core/minikit.ts
@@ -223,7 +223,7 @@ export class MiniKit {
 
       const timestamp = new Date().toISOString();
       const eventPayload: VerifyCommandPayload = {
-        action: encodeAction(payload.action),
+        action: payload.action,
         verification_level: payload.verification_level || VerificationLevel.Orb,
         timestamp,
       };
