c16184fce6d822656b08ac6d748272382aeda8ed
karankurbur <karankurbur@gmail.com>
2025-03-03 22:35:04 -0700

    try catch when compressing proof (#170)
    * try catch when compressing proof
    
    * remove unused file

--- packages/core/helpers/proof/index.ts
+++ packages/core/helpers/proof/index.ts
@@ -2,39 +2,44 @@ import { compressProof } from 'semaphore-rs-js';
 import { decodeAbiParameters, encodeAbiParameters } from 'viem';
 
 export const compressAndPadProof = (proof: `0x${string}`) => {
-  // Decode the hex proof to array of 8 uints
-  const decodedProof = decodeAbiParameters(
-    [{ type: 'uint256[8]' }],
-    proof,
-  )[0] as readonly [
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-  ];
+  try {
+    // Decode the hex proof to array of 8 uints
+    const decodedProof = decodeAbiParameters(
+      [{ type: 'uint256[8]' }],
+      proof,
+    )[0] as readonly [
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+    ];
 
-  // Convert to hex strings for compression
-  const proofHexStrings = [...decodedProof].map(
-    (p) => '0x' + p.toString(16).padStart(64, '0'),
-  ) as [string, string, string, string, string, string, string, string];
+    // Convert to hex strings for compression
+    const proofHexStrings = [...decodedProof].map(
+      (p) => '0x' + p.toString(16).padStart(64, '0'),
+    ) as [string, string, string, string, string, string, string, string];
 
-  // Compress and pad the proof
-  const compressedProof = compressProof(proofHexStrings);
-  const paddedProof = [...compressedProof.map(BigInt), 0n, 0n, 0n, 0n] as [
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-    bigint,
-  ];
+    // Compress and pad the proof
+    const compressedProof = compressProof(proofHexStrings);
+    const paddedProof = [...compressedProof.map(BigInt), 0n, 0n, 0n, 0n] as [
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+      bigint,
+    ];
 
-  // Encode back to hex string
-  return encodeAbiParameters([{ type: 'uint256[8]' }], [paddedProof]);
+    // Encode back to hex string
+    return encodeAbiParameters([{ type: 'uint256[8]' }], [paddedProof]);
+  } catch (e) {
+    console.warn('Failed to compress proof', e);
+    return proof;
+  }
 };
