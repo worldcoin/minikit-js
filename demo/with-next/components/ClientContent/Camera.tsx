bae363e11714c7a006adaee5c852e8aa0686b985
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-03-09 22:03:21 -0700

    test new log

--- demo/with-next/components/ClientContent/Camera.tsx
+++ demo/with-next/components/ClientContent/Camera.tsx
@@ -20,6 +20,7 @@ export const CameraComponent = () => {
   }, []);
 
   const handleMicAccess = useCallback(async () => {
+    console.log('handleMicAccess');
     if (isMicOn) {
       // Stop microphone access
       if (stream) {
