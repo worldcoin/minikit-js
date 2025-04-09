af022280f6cf60968ab850c7133b385479857b15
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-09 15:30:29 -0700

    OAUTH works

--- demo/with-next/app/api/auth-redirect/route.ts
+++ demo/with-next/app/api/auth-redirect/route.ts
@@ -5,13 +5,19 @@ export async function GET(request: NextRequest) {
   const searchParams = new URLSearchParams(url.search);
 
   // Construct the base worldapp URL
-  const worldAppUrl = new URL('worldapp://worldcoin.org/mini-app');
-
+  const worldAppUrl = new URL(
+    `worldapp://worldcoin.org/mini-app?app_id=${process.env.WLD_CLIENT_ID}&path=`,
+  );
+  let path = '/api/auth/callback/google?';
   // Copy all search parameters to the new URL
   searchParams.forEach((value, key) => {
-    worldAppUrl.searchParams.append(key, value);
+    path += `${key}=${value}&`;
   });
+  // Remove the trailing '&'
+  path = path.slice(0, -1);
 
   // Redirect to the constructed URL
-  return NextResponse.redirect(worldAppUrl.toString());
+  return NextResponse.redirect(
+    worldAppUrl.toString() + encodeURIComponent(path),
+  );
 }
