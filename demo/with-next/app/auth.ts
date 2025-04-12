26a89dfa95cc6ce96e8672e40f8a0ac5709f2ea9
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-12 16:33:39 -0700

    bump next version and next auth

--- demo/with-next/app/auth.ts
+++ demo/with-next/app/auth.ts
@@ -1,37 +1,10 @@
-import { NextAuthOptions, User } from 'next-auth';
-import { encode } from 'next-auth/jwt';
-import GoogleProvider from 'next-auth/providers/google';
-
-// Extend the built-in session types
-declare module 'next-auth' {
-  interface Session {
-    user?: User & {
-      verificationLevel?: string;
-    };
-    accessToken?: string;
-    provider?: string;
-  }
-}
-
-// Extend the built-in JWT types
-declare module 'next-auth/jwt' {
-  interface JWT {
-    user?: User & {
-      verificationLevel?: string;
-    };
-    accessToken?: string;
-    provider?: string;
-  }
-}
+import NextAuth, { NextAuthConfig, NextAuthResult } from 'next-auth';
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
-export const authOptions: NextAuthOptions = {
+export const authOptions: NextAuthConfig = {
   session: {
     strategy: 'jwt',
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_SECRET,
   },
   // https://next-auth.js.org/configuration/providers/oauth
   providers: [
@@ -57,10 +30,6 @@ export const authOptions: NextAuthOptions = {
         };
       },
     },
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
   ],
   callbacks: {
     async jwt({ token, account, user }) {
@@ -73,36 +42,6 @@ export const authOptions: NextAuthOptions = {
         token.user = user;
       }
       return token;
-    },
-    async session({ session, token }) {
-      if (token) {
-        session.user = token.user;
-        session.accessToken = token.accessToken;
-        session.provider = token.provider;
-      }
-      return session;
-    },
-    async redirect({ url, baseUrl }) {
-      // If we're coming from Google OAuth callback
-      if (url.includes('/api/auth/callback/google')) {
-        // Get the current session token from the cookie
-        const sessionToken = await encode({
-          secret: process.env.NEXTAUTH_SECRET!,
-          token: {
-            userRole: 'admin',
-            // Include any other session data you need
-          },
-        });
-
-        // Construct the deep link back to the webview
-        const redirectUri = encodeURIComponent(
-          `${process.env.NEXTAUTH_URL}/api/auth-redirect`,
-        );
-        return `worldapp://mini-app?app_id=${process.env.WLD_CLIENT_ID}&path=/api/auth-redirect?token=${sessionToken}&redirect_uri=${redirectUri}`;
-      }
-
-      // For other redirects, use the default behavior
-      return url;
     },
   },
   debug: true,
