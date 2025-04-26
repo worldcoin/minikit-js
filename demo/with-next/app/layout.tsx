028779521c7a203c693d7820126eb9a5f5c00fc5
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-04-25 22:16:15 -0700

    lint

--- demo/with-next/app/layout.tsx
+++ demo/with-next/app/layout.tsx
@@ -1,7 +1,7 @@
+import { auth } from '@/auth';
 import ClientProviders from '@/components/ClientProviders';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { auth } from '@/auth';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
