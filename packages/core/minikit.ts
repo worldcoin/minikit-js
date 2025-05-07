fc167962f030fe048a79b8ea6b7c7e06cebe5efe
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-05-07 15:05:04 -0700

    fix versions

--- packages/core/minikit.ts
+++ packages/core/minikit.ts
@@ -222,9 +222,12 @@ export class MiniKit {
           );
         } else {
           if (commandInput.supported_versions.includes(version)) {
+            // TODO: add proper support for multiple versions
             MiniKit.isCommandAvailable[minikitCommandName] = true;
             isCommandValid = true;
           } else {
+            // For now we will allow the command to be used but we should warn the developer and add granular support for each version
+            isCommandValid = true;
             console.warn(
               `Command ${minikitCommandName} version ${version} is not supported by the app. Supported versions: ${commandInput.supported_versions.join(', ')}. This is not an error, but it is recommended to update the World App version.`,
             );
