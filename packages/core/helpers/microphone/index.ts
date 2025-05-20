210df9f74f3c0f196a34810530d31f731ab0d907
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2025-05-20 16:04:38 -0400

    update

--- packages/core/helpers/microphone/index.ts
+++ packages/core/helpers/microphone/index.ts
@@ -92,8 +92,6 @@ export const setupMicrophone = () => {
     }
   });
 
-  window.__stopAllMiniAppMicrophoneStreams = () =>
-    stopAllMiniAppMicrophoneStreams;
-
+  window.__stopAllMiniAppMicrophoneStreams = stopAllMiniAppMicrophoneStreams;
   microphoneSetupDone = true;
 };
