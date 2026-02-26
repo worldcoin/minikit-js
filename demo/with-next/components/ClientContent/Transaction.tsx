1c23707b9dbb0e84120cc451582d18918624d0fb
Andy Wang <41224501+andy-t-wang@users.noreply.github.com>
2026-02-25 21:08:00 -0800

    test

--- demo/with-next/components/ClientContent/Transaction.tsx
+++ demo/with-next/components/ClientContent/Transaction.tsx
@@ -251,7 +251,7 @@ export const SendTransaction = () => {
               MKT_TOKEN_ADDRESS as `0x${string}`,
               PERMIT_BURN_CONTRACT_ADDRESS as `0x${string}`,
               amount,
-              10000,
+              Math.floor(Date.now() / 1000) + 3600,
             ],
           }),
         },
