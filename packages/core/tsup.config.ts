7d342567572937baddc3be70bb30bbf079e833b4
Takis Kakalis <80459599+Takaros999@users.noreply.github.com>
2026-03-02 11:57:15 -0800

    one tsup config

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,28 +1,18 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig([
-  {
-    dts: true,
-    clean: true,
-    outDir: 'build',
-    format: ['esm', 'cjs'],
-    external: ['wagmi', 'siwe', 'viem', 'react'],
-    entry: [
-      'src/index.ts',
-      'src/connector/index.ts',
-      'src/command-exports.ts',
-      'src/siwe-exports.ts',
-      'src/address-book.ts',
-    ],
-    define: { 'process.env.NODE_ENV': '"production"' },
-  },
-  {
-    dts: true,
-    outDir: 'build',
-    format: ['esm', 'cjs'],
-    external: ['wagmi', 'siwe', 'viem', 'react', 'react-dom'],
-    entry: ['src/minikit-provider.tsx'],
-    banner: { js: "'use client';" },
-    define: { 'process.env.NODE_ENV': '"production"' },
-  },
-]);
+export default defineConfig({
+  dts: true,
+  clean: true,
+  outDir: 'build',
+  format: ['esm', 'cjs'],
+  external: ['wagmi', 'siwe', 'viem', 'react', 'react-dom'],
+  entry: [
+    'src/index.ts',
+    'src/connector/index.ts',
+    'src/command-exports.ts',
+    'src/siwe-exports.ts',
+    'src/address-book.ts',
+    'src/minikit-provider.tsx',
+  ],
+  define: { 'process.env.NODE_ENV': '"production"' },
+});
